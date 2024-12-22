d087267245f8d00caaba8ff7e6392ac445646171
Brais <b.mascat@gmail.com>
2024-12-22 22:43:32 +0100

    feat(src/tests): add parse method and schema for 1 variable

--- src/parser_arguments.py
+++ src/parser_arguments.py
@@ -1,4 +1,17 @@
 
 class ParserArguments:
-    def __init__(self):
-        pass
+    def __init__(self, schema: dict):
+        self.schema = schema
+
+    def parse(self, args: str) -> dict:
+        args_parsed = {}
+        args = args.split("-")
+        for arg in args:
+            if arg == '':
+                continue
+            if len(arg) > 0:
+                key, value = arg.split(" ")
+                args_parsed[key] = value
+            else: 
+                args_parsed[arg] = True
+        return args_parsed
--- tests/test_parser_arguments.py
+++ tests/test_parser_arguments.py
@@ -3,7 +3,13 @@ from src.parser_arguments import ParserArguments
 
 @pytest.fixture(scope="module")
 def parser():
-    return ParserArguments()
+    return ParserArguments(schema={
+        "p": str,
+    })
 
 def test_instance_of_parser_arguments(parser):
     assert isinstance(parser, ParserArguments)
+
+def test_parse_one_argument(parser):
+    list_args = parser.parse("-p value")
+    assert isinstance(list_args["p"], parser.schema["p"])
